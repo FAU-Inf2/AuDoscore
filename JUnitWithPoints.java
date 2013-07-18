f82af42c2636d3be0ddd45c1836f550f3d746495
Norbert Oster <Norbert.Oster@fau.de>
2013-07-18 09:48:51 +0200

    Initial proof of concept code.

---
+++ JUnitWithPoints.java
@@ -0,0 +1,235 @@
+package de.cs.fau.i2.exercises;
+
+import java.util.*;
+import java.io.*;
+import java.lang.annotation.*;
+
+import org.junit.*;
+import org.junit.rules.*;
+import org.junit.runner.*;
+import org.junit.runners.model.*;
+
+public abstract class JUnitWithPoints {
+	// ******************** RULES **************************************** //
+	@Rule
+	public final PointsLogger pointsLogger = new PointsLogger();
+	@ClassRule
+	public final static PointsSummary pointsSummary = new PointsSummary();
+
+	// ******************** ANNOTATIONS **************************************** //
+	@Inherited
+	@Target(java.lang.annotation.ElementType.TYPE)
+	@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+	public static @interface Exercises {
+		Ex[] value();
+	}
+
+	@Inherited
+	@Target(java.lang.annotation.ElementType.TYPE)
+	@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+	public static @interface Ex {
+		String exID();
+
+		double points();
+
+		String comment() default "<n.a.>";
+	}
+
+	// -------------------------------------------------------------------------------- //
+	@Inherited
+	@Target(java.lang.annotation.ElementType.METHOD)
+	@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+	public static @interface Bonus {
+		String exID();
+
+		double bonus();
+
+		String comment() default "<n.a.>";
+	}
+
+	@Inherited
+	@Target(java.lang.annotation.ElementType.METHOD)
+	@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+	public static @interface Malus {
+		String exID();
+
+		double malus();
+
+		String comment() default "<n.a.>";
+	}
+
+	// ******************** BACKEND FUNCTIONALITY **************************************** //
+	private static final HashMap<String, Ex> exerciseHashMap = new HashMap<>();
+	private static final HashMap<String, List<ReportEntry>> reportHashMap = new HashMap<>();
+
+	// -------------------------------------------------------------------------------- //
+	private static final class ReportEntry {
+		Description description;
+		Bonus bonus;
+		Malus malus;
+		Throwable throwable;
+
+		private ReportEntry(Description description, Bonus bonus, Malus malus, Throwable throwable) {
+			this.description = description;
+			this.bonus = bonus;
+			this.malus = malus;
+			this.throwable = throwable;
+		}
+
+		final String format(double bonusDeclaredPerExercise, double pointsDeclaredPerExercise) {
+			String result = "";
+			if (bonus != null) {
+				if (throwable != null) {
+					result += String.format("%1$10.3f", 0.0);
+				} else {
+					result += String.format("%1$+10.3f", (pointsDeclaredPerExercise * Math.abs(bonus.bonus()) / bonusDeclaredPerExercise));
+				}
+				result += " | " + bonus.comment();
+				result += " | " + description.getDisplayName();
+				if (throwable != null) {
+					result += " | " + throwable.getClass().getName() + "(" + throwable.getLocalizedMessage() + ")";
+				} else {
+					result += " | <passed>";
+				}
+			}
+			if (malus != null) {
+				if (throwable != null) {
+					result += String.format("%1$+10.3f", -(pointsDeclaredPerExercise * Math.abs(malus.malus()) / bonusDeclaredPerExercise));
+				} else {
+					result += String.format("%1$10.3f", 0.0);
+				}
+				result += " | " + malus.comment();
+				result += " | " + description.getDisplayName();
+				if (throwable != null) {
+					result += " | " + throwable.getClass().getName() + "(" + throwable.getLocalizedMessage() + ")";
+				} else {
+					result += " | <passed>";
+				}
+			}
+			return result;
+		}
+	}
+
+	// -------------------------------------------------------------------------------- //
+	public static final class PointsLogger extends TestWatcher {
+		@Override
+		protected final void failed(Throwable throwable, Description description) {
+			Bonus bonusAnnotation = description.getAnnotation(Bonus.class);
+			Malus malusAnnotation = description.getAnnotation(Malus.class);
+			if (bonusAnnotation == null && malusAnnotation == null) {
+				throw new AnnotationFormatError("WARNING - found test case without BONUS or MALUS annotation: [" + description.getDisplayName() + "]");
+			} else if (bonusAnnotation != null && bonusAnnotation.exID().trim().length() == 0) {
+				throw new AnnotationFormatError("WARNING - found test case with empty exercise id in BONUS annotation: [" + description.getDisplayName() + "]");
+			} else if (bonusAnnotation != null && !exerciseHashMap.containsKey(bonusAnnotation.exID())) {
+				throw new AnnotationFormatError("WARNING - found test case with non-declared exercise id in BONUS annotation: [" + description.getDisplayName() + "]");
+			} else if (bonusAnnotation != null && bonusAnnotation.bonus() == 0) {
+				throw new AnnotationFormatError("WARNING - found test case with illegal bonus value in BONUS annotation: [" + description.getDisplayName() + "]");
+			} else if (malusAnnotation != null && malusAnnotation.exID().trim().length() == 0) {
+				throw new AnnotationFormatError("WARNING - found test case with empty exercise id in MALUS annotation: [" + description.getDisplayName() + "]");
+			} else if (malusAnnotation != null && !exerciseHashMap.containsKey(malusAnnotation.exID())) {
+				throw new AnnotationFormatError("WARNING - found test case with non-declared exercise id in MALUS annotation: [" + description.getDisplayName() + "]");
+			} else if (malusAnnotation != null && malusAnnotation.malus() == 0) {
+				throw new AnnotationFormatError("WARNING - found test case with illegal malus value in MALUS annotation: [" + description.getDisplayName() + "]");
+			} else {
+				if (bonusAnnotation != null) {
+					if (!reportHashMap.containsKey(bonusAnnotation.exID())) {
+						reportHashMap.put(bonusAnnotation.exID(), new ArrayList<ReportEntry>());
+					}
+					reportHashMap.get(bonusAnnotation.exID()).add(new ReportEntry(description, bonusAnnotation, null, throwable));
+				}
+				if (malusAnnotation != null) {
+					if (!reportHashMap.containsKey(malusAnnotation.exID())) {
+						reportHashMap.put(malusAnnotation.exID(), new ArrayList<ReportEntry>());
+					}
+					reportHashMap.get(malusAnnotation.exID()).add(new ReportEntry(description, null, malusAnnotation, throwable));
+				}
+			}
+		}
+
+		@Override
+		protected final void succeeded(Description description) {
+			failed(null, description);
+		}
+	}
+
+	// -------------------------------------------------------------------------------- //
+	public static final class PointsSummary extends ExternalResource {
+		@Override
+		public Statement apply(Statement base, Description description) {
+			reportHashMap.clear();
+			exerciseHashMap.clear();
+			Exercises exercisesAnnotation = description.getAnnotation(Exercises.class);
+			if (exercisesAnnotation == null || exercisesAnnotation.value().length == 0) {
+				throw new AnnotationFormatError("WARNING - found test set without exercise points declaration: [" + description.getDisplayName() + "]");
+			}
+			for (Ex exercise : exercisesAnnotation.value()) {
+				if (exercise.exID().trim().length() == 0) {
+					throw new AnnotationFormatError("WARNING - found exercise points declaration with empty exercise name and following points: [" + exercise.points() + "]");
+				} else if (exercise.points() == 0) {
+					throw new AnnotationFormatError("WARNING - found exercise points declaration with illegal points value: [" + exercise.exID() + "]");
+				} else if (exerciseHashMap.containsKey(exercise.exID())) {
+					throw new AnnotationFormatError("WARNING - found exercise points declaration with duplicate exercise: [" + exercise.exID() + "]");
+				}
+				exerciseHashMap.put(exercise.exID(), exercise);
+			}
+			return super.apply(base, description);
+		}
+
+		@Override
+		protected final void after() {
+			for (String exerciseId : exerciseHashMap.keySet()) {
+				if (!reportHashMap.containsKey(exerciseId)) {
+					throw new AnnotationFormatError("WARNING - found exercise points declaration for exercise without test case: [" + exerciseId + "]");
+				}
+			}
+			String[] exerciseIds = reportHashMap.keySet().toArray(new String[0]);
+			Arrays.sort(exerciseIds);
+			String summary = "";
+			double pointsAchievedTotal = 0, bonusDeclaredPerExercise, bonusAchievedPerExercise, pointsDeclaredPerExercise, pointsAchievedPerExercise;
+			for (String exerciseId : exerciseIds) {
+				Ex exercise = exerciseHashMap.get(exerciseId);
+				List<ReportEntry> reportPerExercise = reportHashMap.get(exerciseId);
+				summary += "========== START OF TEST RESULTS FOR EXERCISE \"" + exercise.exID() + "\" ==========" + "\n";
+				summary += "    POINTS | COMMENT | TEST CASE (jUnit method name + test class) | TEST RESULT (jUnit assertion)" + "\n";
+				summary += "  -----------------------------------------------------------------------------------------------" + "\n";
+				bonusDeclaredPerExercise = 0;
+				for (ReportEntry reportEntry : reportPerExercise) {
+					if (reportEntry.bonus != null) {
+						bonusDeclaredPerExercise += Math.abs(reportEntry.bonus.bonus());
+					}
+				}
+				pointsDeclaredPerExercise = exercise.points();
+				bonusAchievedPerExercise = 0;
+				for (ReportEntry reportEntry : reportPerExercise) {
+					Bonus bonus = reportEntry.bonus;
+					Malus malus = reportEntry.malus;
+					Throwable throwable = reportEntry.throwable;
+					summary += reportEntry.format(bonusDeclaredPerExercise, pointsDeclaredPerExercise) + "\n";
+					if (bonus != null && throwable == null) {
+						bonusAchievedPerExercise += Math.abs(bonus.bonus());
+					}
+					if (malus != null && throwable != null) {
+						bonusAchievedPerExercise -= Math.abs(malus.malus());
+					}
+				}
+				summary += "  -----------------------------------------------------------------------------------------------" + "\n";
+				bonusAchievedPerExercise = Math.min(bonusDeclaredPerExercise, Math.max(0, bonusAchievedPerExercise));
+				pointsAchievedPerExercise = Math.ceil(pointsDeclaredPerExercise * 2 * bonusAchievedPerExercise / bonusDeclaredPerExercise) / 2;
+				summary += String.format(" >%1$6.1f", pointsAchievedPerExercise) + "\n";
+				summary += "========== END OF TEST RESULTS FOR EXERCISE \"" + exercise.exID() + "\" ==========" + "\n \n";
+				pointsAchievedTotal += pointsAchievedPerExercise;
+			}
+			summary += "========== TOTAL SUMMARY: ==========" + "\n";
+			summary += "TOTAL POINTS: " + pointsAchievedTotal + "\n";
+			summary += "====================================" + "\n";
+			summary = "points: " + String.format("%1$.1f", pointsAchievedTotal) + "\n\n" + summary;
+			try {
+				try (FileWriter fileWriter = new FileWriter(new File(System.getProperty("user.dir"), "autocomment.txt"))) {
+					fileWriter.write(summary);
+				}
+			} catch (Throwable t) {
+				System.err.println(summary);
+			}
+		}
+	}
+}
---
+++ SystemUnderTest.java
@@ -0,0 +1,17 @@
+public class SystemUnderTest {
+	public int foo() {
+		return 4711;
+	}
+
+	public double bar() {
+		return 4711.0815;
+	}
+
+	public String baz() {
+		return "I am nice.";
+	}
+
+	public String foobar() {
+		return "I am dangerous.";
+	}
+}
