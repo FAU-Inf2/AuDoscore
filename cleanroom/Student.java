f315354ee50eca91e5029f02e9547a3ee69d383a
Daniel Brinkers <daniel.brinkers@cs.fau.de>
2013-09-26 12:37:17 +0200

    Add (wrong) cleanroom solution.
    Still have to implement it right.

---
+++ cleanroom/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+	public int foo() {
+		return 4711;
+	}
+
+	public double bar() {
+		return 4711.0815;
+	}
+
+	public String baz() {
+		return "I am nice.";
+	}
+
+	public String foobar() {
+		return "I am dangerous.";
+	}
+
+	public static Object getNull() {
+		return null;
+	}
+
+	public static String doNull() {
+		return getNull().toString();
+	}
+
+	public static void recur() {
+		recur();
+	}
+
+	public static void ioob() {
+		int a[] = new int[10];
+		a[a.length + 32]++;
+	}
+}
