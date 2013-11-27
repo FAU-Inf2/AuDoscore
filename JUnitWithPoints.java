caf2c012a6d398ac6b2046119eaac05f6b1a3a54
Daniel Brinkers <daniel.brinkers@cs.fau.de>
2013-11-27 15:15:06 +0100

    Add classloader experiment.

--- JUnitWithPoints.java
+++ JUnitWithPoints.java
@@ -240,6 +240,104 @@ public abstract class JUnitWithPoints {
 
 		private PrintStream saveOut;
 		private PrintStream saveErr;
+		public static class Cl extends ClassLoader {
+
+			/**
+			 * Parent ClassLoader passed to this constructor
+			 * will be used if this ClassLoader can not resolve a
+			 * particular class.
+			 *
+			 * @param parent Parent ClassLoader
+			 *              (may be from getClass().getClassLoader())
+			 */
+			public Cl(ClassLoader parent) {
+				super(parent);
+			}
+
+			private String prefix = "";
+
+			public void setPrefix(String prefix){
+				if(!prefix.equals("")){
+					prefix += File.separatorChar;
+				}
+				this.prefix = prefix;
+			}
+
+			/**
+			 * Loads a given class from .class file just like
+			 * the default ClassLoader. This method could be
+			 * changed to load the class over network from some
+			 * other server or from the database.
+			 *
+			 * @param name Full class name
+			 */
+			private Class<?> getClass(String name)
+				throws ClassNotFoundException {
+				// We are getting a name that looks like
+				// javablogging.package.ClassToLoad
+				// and we have to convert it into the .class file name
+				// like javablogging/package/ClassToLoad.class
+				String file = prefix + name.replace('.', File.separatorChar) + ".class";
+				byte[] b = null;
+				try {
+					// This loads the byte code data from the file
+					b = loadClassData(file);
+					// defineClass is inherited from the ClassLoader class
+					// and converts the byte array into a Class
+					Class<?> c = defineClass(name, b, 0, b.length);
+					resolveClass(c);
+					return c;
+				} catch (IOException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+
+			/**
+			 * Every request for a class passes through this method.
+			 * If the requested class is in "javablogging" package,
+			 * it will load it using the
+			 * {@link CustomClassLoader#getClass()} method.
+			 * If not, it will use the super.loadClass() method
+			 * which in turn will pass the request to the parent.
+			 *
+			 * @param name
+			 *            Full class name
+			 */
+			@Override
+			public Class<?> loadClass(String name)
+			throws ClassNotFoundException {
+			System.out.println("loading class '" + name + "'");
+			if (name.startsWith("A") || name.startsWith("Cl")) {
+				return getClass(name);
+			}
+			return super.loadClass(name);
+			}
+
+			/**
+			 * Loads a given file (presumably .class) into a byte array.
+			 * The file should be accessible as a resource, for example
+			 * it could be located on the classpath.
+			 *
+			 * @param name File name to load
+			 * @return Byte array read from the file
+			 * @throws IOException Is thrown when there
+			 *               was some problem reading the file
+			 */
+			private byte[] loadClassData(String name) throws IOException {
+				// Opening the file
+				System.out.println("loading " + name);
+				InputStream stream = getClass().getClassLoader()
+					.getResourceAsStream(name);
+				int size = stream.available();
+				byte buff[] = new byte[size];
+				DataInputStream in = new DataInputStream(stream);
+				// Reading the binary data
+				in.readFully(buff);
+				in.close();
+				return buff;
+			}
+		}
 
 		@Override
 		protected void starting(Description description) {
@@ -273,6 +371,8 @@ public abstract class JUnitWithPoints {
 				Method setReplace = cl.loadClass("asp.Config").getMethod("setReplace", Method.class, Method.class);
 				Factory.mClassMap = new HashMap<Class, Class>();
 				Factory.mMethsMap = new HashMap<String, SortedSet<String>>();
+
+				Cl rcl = new Cl(Cl.class.getClassLoader());
 				if(description.getAnnotation(Replace.class) != null) {
 					Replace r = description.getAnnotation(Replace.class);
 					for(int i=0; i<r.value().length; ++i){
@@ -280,7 +380,7 @@ public abstract class JUnitWithPoints {
 						String cln = r.value()[i].substring(0, s);
 
 						String regex = r.value()[i].substring(s+1);
-						
+
 						if(!Factory.mMethsMap.containsKey(cln))
 							Factory.mMethsMap.put(cln, new TreeSet<String>());
 						SortedSet<String> meths = Factory.mMethsMap.get(cln);
@@ -295,7 +395,8 @@ public abstract class JUnitWithPoints {
 						String ncln = e.getKey();
 						for(String me : e.getValue())
 							ncln += "_" + me;
-						Factory.mClassMap.put(cl.loadClass(e.getKey()), cl.loadClass(ncln));
+						rcl.setPrefix(ncln);
+						Factory.mClassMap.put(cl.loadClass(e.getKey()), rcl.loadClass(e.getKey()));
 					}
 
 					for(int i=0; i<r.value().length; ++i){
@@ -303,7 +404,7 @@ public abstract class JUnitWithPoints {
 						String cln = r.value()[i].substring(0, s);
 
 						String regex = r.value()[i].substring(s+1);
-						
+
 						for(Method me : cl.loadClass(cln).getDeclaredMethods()){
 							if((me.getModifiers() & Modifier.STATIC) == 0)
 								continue;
