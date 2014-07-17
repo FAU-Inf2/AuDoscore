39f5dfacf57a336d5e2ad2a8df59de012af877a5
Tobias Werth <werth@cs.fau.de>
2014-07-17 17:14:01 +0200

    Add simple testing scripts.

---
+++ verify_single.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+( ./run_test.sh ) > /dev/null 2> /dev/null
+
+error=0
+for i in expected/*; do
+	diff $i test.latest/$(basename "$i")
+	ec=$?
+	error=$((error|ec))
+done;
+
+rm -rf $(readlink -f test.latest)
+rm test.latest
+
+exit $error
