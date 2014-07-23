2720f089099e3bc1954ffebaad356fd42fa6d91b
Tobias Werth <werth@cs.fau.de>
2014-07-23 12:31:55 +0200

    Allow directories in expected files for tests.

--- verify_single.sh
+++ verify_single.sh
@@ -3,8 +3,9 @@
 ( ./run_test.sh ) > /dev/null 2> /dev/null
 
 error=0
-for i in expected/*; do
-	diff -u -I '^make' -I '^Makefile:' $i test.latest/$(basename "$i")
+for i in `find expected -type f`; do
+	testfile=${i/expected/test.latest}
+	diff -u -I '^make' -I '^Makefile:' $i $testfile
 	ec=$?
 	error=$((error|ec))
 done;
