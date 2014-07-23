6b52f73bc9b2370d4b4620804786f73f53cb6f8c
Tobias Werth <werth@cs.fau.de>
2014-07-23 16:32:18 +0200

    Add more test cases for forbidden.

---
+++ tests/forbidden_math/student/ToTest.java
@@ -0,0 +1,5 @@
+public class ToTest {
+	public static int toTest() {
+		return Math.max(42,23);
+	}
+}
---
+++ tests/forbidden_max_but_min/student/ToTest.java
@@ -0,0 +1,5 @@
+public class ToTest {
+	public static int toTest() {
+		return Math.min(42,42+23);
+	}
+}
