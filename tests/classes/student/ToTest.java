da109c33e9f1c2fb50ba152f0491d8554619dcda
Tobias Werth <werth@cs.fau.de>
2014-07-25 15:36:08 +0200

    Add test file which uses the same class names as the annotations.

---
+++ tests/classes/student/ToTest.java
@@ -0,0 +1,44 @@
+public class ToTest {
+	public static int toTest() {
+		return Exercises.foo();
+	}
+}
+class Exercises {
+	public static int foo() {
+		return new Ex().foo();
+	}
+}
+class Ex {
+	public int foo() {
+		return new Bonus(42).bar();
+	}
+}
+class Bonus {
+	int bonus;
+	Bonus(int bonus) {
+		this.bonus = bonus;
+	}
+	public int bar() {
+		return Malus.bar(-bonus);
+	}
+}
+class Malus {
+	public static int bar(int malus) {
+		return Forbidden.forbid(-malus);
+	}
+}
+class Forbidden {
+	public static int forbid(int x) {
+		return NotForbidden.minusOne() * (-x) + SecretCase.nothing();
+	}
+}
+class NotForbidden {
+	public static int minusOne() {
+		return -1;
+	}
+}
+class SecretCase {
+	public static int nothing() {
+		return 0;
+	}
+}
