FAU-Inf2/AuDoscore

commit c730019fd75e8ee2135823d87c3e315390206e20

Move annotations to own package.
A student might use the same class names now.

Makefile
@@ -31,7 +31,7 @@ prepare: updategitrev lib/junitpoints.jar
 updategitrev:
 	git rev-parse HEAD > GITREV
 
-SRCJUNITPOINTSJAR := JUnitWithPoints.java tester/Replace.java JUnitPointsMerger.java tester/ReadReplace.java ReadForbidden.java ReplaceMixer.java
+SRCJUNITPOINTSJAR := JUnitWithPoints.java tester/Replace.java JUnitPointsMerger.java tester/ReadReplace.java ReadForbidden.java ReplaceMixer.java tester/annotations/Bonus.java tester/annotations/Ex.java tester/annotations/Exercises.java tester/annotations/Forbidden.java tester/annotations/Malus.java tester/annotations/NotForbidden.java tester/annotations/SecretCase.java
 
 lib/junitpoints.jar: build $(SRCJUNITPOINTSJAR)
 	javac -d build -cp lib/json-simple-1.1.1.jar:lib/junit.jar:lib/tools.jar:. $(SRCJUNITPOINTSJAR)
@@ -43,7 +43,7 @@ compile-stage0:
 compile-stage1: miniclean
 	cp $(TEST).java $(TEST).java.orig
 	( /bin/echo -e "import org.junit.*;\n import tester.*;\n" ; cat $(TEST).java.orig ) > $(TEST).java
-	sed -i -e 's/@SecretCase/@Ignore/' $(TEST).java
+	sed -i -e 's/@tester.annotations.SecretCase/@Ignore/' $(TEST).java
 	make -B $(TESTCLASS) || ( mv $(TEST).java.orig $(TEST).java; /bin/false; )
 	mv $(TEST).java.orig $(TEST).java
 	java -cp lib/junitpoints.jar:. ReadForbidden $(TEST) > forbidden

JUnitWithPoints.java
@@ -12,68 +12,8 @@ import org.json.simple.parser.*;
 
 import java.lang.reflect.*;
 import tester.*;
+import tester.annotations.*;
 
-// ******************** ANNOTATIONS **************************************** //
-@Inherited
-@Target(java.lang.annotation.ElementType.TYPE)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface Exercises {
-	Ex[] value();
-}
-
-@Inherited
-@Target(java.lang.annotation.ElementType.TYPE)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface Ex {
-	String exID();
-
-	double points();
-
-	String comment() default "<n.a.>";
-}
-
-@Inherited
-@Target(java.lang.annotation.ElementType.TYPE)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface Forbidden {
-	String[] value();
-}
-
-@Inherited
-@Target(java.lang.annotation.ElementType.TYPE)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface NotForbidden {
-	String[] value();
-}
-
-// -------------------------------------------------------------------------------- //
-@Inherited
-@Target(java.lang.annotation.ElementType.METHOD)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface Bonus {
-	String exID();
-
-	double bonus();
-
-	String comment() default "<n.a.>";
-}
-
-@Inherited
-@Target(java.lang.annotation.ElementType.METHOD)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface Malus {
-	String exID();
-
-	double malus();
-
-	String comment() default "<n.a.>";
-}
-
-@Inherited
-@Target(java.lang.annotation.ElementType.METHOD)
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface SecretCase {
-}
 
 // ******************** RULES HELPER for pretty code **************************************** //
 final class PointsLogger extends JUnitWithPoints.PointsLogger {

ReadForbidden.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.lang.reflect.*;
 import java.lang.annotation.*;
+import tester.annotations.*;
 
 public class ReadForbidden {
 	public static void main(String args[]) throws Exception {

tester/annotations/Bonus.java
@@ -1,0 +1,15 @@
+package tester.annotations;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Target(java.lang.annotation.ElementType.METHOD)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface Bonus {
+	String exID();
+
+	double bonus();
+
+	String comment() default "<n.a.>";
+}
+

tester/annotations/Ex.java
@@ -1,0 +1,15 @@
+package tester.annotations;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Target(java.lang.annotation.ElementType.TYPE)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface Ex {
+	String exID();
+
+	double points();
+
+	String comment() default "<n.a.>";
+}
+

tester/annotations/Exercises.java
@@ -1,0 +1,11 @@
+package tester.annotations;
+
+import java.lang.annotation.*;
+
+// ******************** ANNOTATIONS **************************************** //
+@Inherited
+@Target(java.lang.annotation.ElementType.TYPE)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface Exercises {
+	Ex[] value();
+}

tester/annotations/Forbidden.java
@@ -1,0 +1,11 @@
+package tester.annotations;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Target(java.lang.annotation.ElementType.TYPE)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface Forbidden {
+	String[] value();
+}
+

tester/annotations/Malus.java
@@ -1,0 +1,15 @@
+package tester.annotations;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Target(java.lang.annotation.ElementType.METHOD)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface Malus {
+	String exID();
+
+	double malus();
+
+	String comment() default "<n.a.>";
+}
+

commit 2ee9d24add348ab97cd01d4d4dfc59077e61a94a

Fixed compiler errors due to subpackage
commit c730019fd75e8ee2135823d87c3e315390206e20
introduced bug in Makefile after moving
annotations into own package

Makefile
@@ -42,7 +42,7 @@ compile-stage0:
 
 compile-stage1: miniclean
 	cp $(TEST).java $(TEST).java.orig
-	( /bin/echo -e "import org.junit.*;\n import tester.*;\n" ; cat $(TEST).java.orig ) > $(TEST).java
+	( /bin/echo -e "import org.junit.*;\nimport tester.*;\nimport tester.annotations.*;\n" ; cat $(TEST).java.orig ) > $(TEST).java
 	sed -i -e 's/@tester.annotations.SecretCase/@Ignore/' $(TEST).java
 	make -B $(TESTCLASS) || ( mv $(TEST).java.orig $(TEST).java; /bin/false; )
 	mv $(TEST).java.orig $(TEST).java
@@ -54,7 +54,7 @@ compile-stage1: miniclean
 compile-stage2: miniclean
 	./obfuscate
 	cp $(TEST).java $(TEST).java.orig
-	( /bin/echo -e "import org.junit.*;\n import tester.*;\n" ; cat $(TEST).java.orig ) > $(TEST).java
+	( /bin/echo -e "import org.junit.*;\nimport tester.*;\nimport tester.annotations.*;\n" ; cat $(TEST).java.orig ) > $(TEST).java
 	make -B $(TESTCLASS) || ( mv $(TEST).java.orig $(TEST).java; /bin/false; )
 	mkdir -p mixed || ( mv $(TEST).java.orig $(TEST).java; /bin/false; )
 	java -cp lib/junitpoints.jar:lib/junit.jar:. tester.ReadReplace $(TEST) > compile2.sh || ( mv $(TEST).java.orig $(TEST).java; /bin/false; )