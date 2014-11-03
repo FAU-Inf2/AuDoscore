8f622d20e4f047e69c47c10c9d56f5c679918191
Tobias Werth <werth@cs.fau.de>
2014-11-03 13:04:42 +0100

    Add testcase for #2, should fail, but does not.

---
+++ tests/shuffled_json/junit/UnitTest.java
@@ -0,0 +1,32 @@
+import org.junit.*;
+import tester.*;
+
+import org.junit.*;
+import org.junit.rules.*;
+import static org.junit.Assert.*;
+import java.lang.reflect.*;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+@tester.annotations.Exercises({ @tester.annotations.Ex(exID = "GA4.6a", points = 12.5)})
+public class UnitTest {
+	// instead of explicitly coding the following rules here,
+	// your test class can also just extend the class JUnitWithPoints
+	@Rule
+	public final PointsLogger pointsLogger = new PointsLogger();
+	@ClassRule
+	public final static PointsSummary pointsSummary = new PointsSummary();
+
+	@Test(timeout=100)
+	@tester.annotations.Bonus(exID = "GA4.6a", bonus = 47)
+	public void test() {
+		assertEquals("Should return 42", 42, ToTest.toTest());
+	}
+
+	@Test(timeout=100)
+	@tester.annotations.Malus(exID = "GA4.6a", malus = 11)
+	public void test2() {
+		assertEquals("Should return 23", 23, ToTest.toTest2());
+	}
+}
