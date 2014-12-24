c7cb5763fc108174c78241042e57c915ef1925f9
Cuong Bui <cuong.bui@fau.de>
2014-12-24 11:08:04 +0100

    merging with public/secret test, #20

--- test.sh
+++ test.sh
@@ -224,7 +224,13 @@ info "  json:"
 cat run2.err
 
 info "- merging"
-( java -cp lib/junitpoints.jar:lib/json-simple-1.1.1.jar JUnitPointsMerger run2.err merged ) > merge 2>&1
+if [ "x$secretclass" != "x" ]; then
+	( java -cp lib/junitpoints.jar:lib/json-simple-1.1.1.jar JUnitPointsMerger -Dpub=$testclass -Dsecret=$secretclass run2.err merged ) > merge 2>&1
+else
+	( java -cp lib/junitpoints.jar:lib/json-simple-1.1.1.jar JUnitPointsMerger run2.err merged ) > merge 2>&1
+
+fi
+
 checkexit $? "\ninternal error\n" merge
 cat merged
 
