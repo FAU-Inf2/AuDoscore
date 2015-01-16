5ad5d9bcdccc48e9f2e86d16e34a357fcce3fe96
Cuong Bui <cuong.bui@fau.de>
2015-01-16 10:49:50 +0100

    removed duplicated tests, removed tests with @Replace in pub test, converted old tests, #20

--- tests/replaced_clean_variable/cleanroom/ToTest.java
+++
@@ -1,6 +0,0 @@
-public class ToTest {
-	static int __clean = 42;
-	public static int toTest() {
-		return __clean;
-	}
-}
--- tests/replaced_clean_variable/student/ToTest.java
+++
@@ -1,7 +0,0 @@
-public class ToTest {
-	static int __clean = 23;
-	static int __clean2 = 42;
-	public static int toTest() {
-		return __clean + __clean2;
-	}
-}
