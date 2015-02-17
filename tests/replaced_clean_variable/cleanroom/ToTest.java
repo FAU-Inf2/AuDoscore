8e332a25e904135120e0ef77e88c3492b5f52812
Cuong Bui <cuong.bui@fau.de>
2015-02-17 22:38:07 +0100

    replaced_clean resurrected

---
+++ tests/replaced_clean_variable/cleanroom/ToTest.java
@@ -0,0 +1,6 @@
+public class ToTest {
+	static int __clean = 42;
+	public static int toTest() {
+		return __clean;
+	}
+}
---
+++ tests/replaced_clean_variable/student/ToTest.java
@@ -0,0 +1,7 @@
+public class ToTest {
+	static int __clean = 23;
+	static int __clean2 = 42;
+	public static int toTest() {
+		return __clean + __clean2;
+	}
+}
