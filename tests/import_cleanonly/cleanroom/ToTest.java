499821c97e2f79305a3df95be976a5a6e2b4e8b5
Cuong Bui <cuong.bui@fau.de>
2015-02-17 21:47:18 +0100

    added import cleanonly test

---
+++ tests/import_cleanonly/cleanroom/ToTest.java
@@ -0,0 +1,13 @@
+import java.math.*;
+
+public class ToTest {
+	public static int toTest() {
+		return 42;
+	}
+	public static int toTest2() {
+		return 23;
+	}
+	public static int third() {
+		return new BigInteger("3").intValue();
+	}
+}
