02e5a1bf59bea6bb80d41ba684fabac382cf632e
Cuong Bui <cuong.bui@fau.de>
2015-04-13 13:28:11 +0200

    some build errors gone, #12

--- tools/ic/InterfaceComparer.java
+++ tools/ic/InterfaceComparer.java
@@ -1,13 +1,15 @@
+import java.util.*;
+import java.lang.reflect.*;
 public class InterfaceComparer {
 	private static void usage() {
 		System.err.println("java InterfaceComparer <CleanroomClass> <StudentClass>");
 		System.exit(1);
 	}
 
-	private static compareClasses(Class cleanroomClass, Class studentClass){
+	private static void compareClasses(Class cleanroomClass, Class studentClass) {
 	
 		boolean equals = false;
-		ArrayList<Method> studentMethods = new ArrayList<Method>(studentClass.getMethods());
+		ArrayList<Method> studentMethods = new ArrayList<Method>(Arrays.asList(studentClass.getMethods()));
 		// checkMethods
 		for(Method cleanroomMethod : cleanroomClass.getMethods()){
 			for(Method studentMethod : studentMethods) {
@@ -34,13 +36,16 @@ public class InterfaceComparer {
 		Class cleanroomClass = null;
 		Class studentClass = null;
 
-		ClassLoader cl = ClassLoader.getSystemClassLoade();
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
 		try{
 			cleanroomClass = cl.loadClass(args[0]);
-			studentClass = cl.loadClass(args[2]);
+			studentClass = cl.loadClass(args[1]);
 
-			compareClasses(cleanroomClass,studentClass);
+		} catch (ClassNotFoundException cnfe) {
+			throw new Error("WARNING - " + cnfe.getMessage());
 		}
+		
+		compareClasses(cleanroomClass,studentClass);
 
 	}
 
