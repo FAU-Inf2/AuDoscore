0bc126ccc426011c6119468a5565707c175830bc
Tobias Werth <werth@cs.fau.de>
2015-04-30 12:15:41 +0200

    Use own class to run single methods instead of ignoring all others. WIP.

---
+++ tools/SingleMethodRunner.java
@@ -0,0 +1,24 @@
+package tools;
+
+import org.junit.internal.RealSystem;
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+
+public class SingleMethodRunner {
+    public static void main(String... args) throws ClassNotFoundException {
+        if (args.length != 2) {
+            System.err.println("Usage: class method FIXME");
+            return;
+        }
+
+        String clazz = args[0];
+        String method = args[1];
+        Request request = Request.method(Class.forName(clazz), method);
+        JUnitCore juc = new JUnitCore();
+        juc.addListener(new TextListener(new RealSystem()));
+        Result result = juc.run(request);
+        System.exit(result.wasSuccessful() ? 0 : 1);
+    }
+}
