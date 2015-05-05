63dfd919385ed71f7793109176e8da299977d80f
Cuong Bui <cuong.bui@fau.de>
2015-05-05 08:39:52 +0200

    now able to check multiple classes, #12

--- tools/ic/InterfaceComparer.java
+++ tools/ic/InterfaceComparer.java
@@ -9,9 +9,10 @@ import java.io.*;
 
 public class InterfaceComparer {
 	private static boolean error = false;
-	private static HashMap<String,Boolean> methodMap = null;
+	private static HashMap<String,Boolean> checkMap = null;
 	private static void compareClasses(Class<?> cleanroomClass, Class<?> studentClass) {
 		boolean equals = false;
+		HashMap<String,Boolean> methodMap = checkMap.get(cleanroomClass.getName());
 		ArrayList<Method> studentMethods = new ArrayList<Method>(Arrays.asList(studentClass.getDeclaredMethods()));
 		// checkMethods
 		for(Method cleanroomMethod : cleanroomClass.getDeclaredMethods()){
@@ -44,18 +45,30 @@ public class InterfaceComparer {
 		return idx >= 0 ? path.substring(idx + 1) : path;
 	}
 
-	private static HashMap<String,Boolean> argsToMap(String[] methods){
-		HashMap<String,Boolean> methodMap = new HashMap<String,Boolean>();
-		for(String method : methods){
-			methodMap.put(method,true);		
-		}
+	private static HashMap<String,Boolean> argsToMap(String[] args){
+		HashMap<String,HashMap<String,Boolean>> checkMap = new HashMap<String,HashMap<String,Boolean>>();
+		for(String cm : args){
+			if(cm.contains(".")){
+				String[] parts = cm.split(".");
+				HashMap<String,String> methodMap = checkMap.get(parts[0]);
+				if(classMethodMap == null){
+					methodMap = new HashMap<String,Boolean>();
+					methodMap.put(parts[1],true);
+					checkMap.put(parts[0],methodMap);
+				}else{
+					methodMap.put(parts[1],true);
+				}
+			} else {
+				throw new IllegalArgumentException(/*TODO*/);
+			}
 
-		return methodMap;
+		}
+		return checkMap;
 	}
 
 	public static void main(String args[]){
 		if(args == null){
-			System.err.println("Usage: java tools.ic.InterfaceComparer <method1> .. <methodn>");
+			System.err.println("Usage: java tools.ic.InterfaceComparer <Class.method1> .. <Class.methodn>");
 			System.exit(-1);
 		}
 		String cwd = System.getProperty("user.dir");
@@ -64,40 +77,30 @@ public class InterfaceComparer {
 		ClassLoader cl = ClassLoader.getSystemClassLoader();
 		ClassLoader cleanroomLoader = null;
 		ClassLoader studentLoader = null;
-		methodMap = argsToMap(args);
+		checkMap = argsToMap(args);
 		try{
 			cleanroomLoader = new URLClassLoader(new URL[]{new File(pathToCleanroom).toURI().toURL()});
 			studentLoader = new URLClassLoader(new URL[]{new File(cwd).toURI().toURL()});
 		}catch(MalformedURLException mfue){
 			throw new Error("Error"  + mfue.getMessage());
 		}
-		for(File path : f.listFiles()) {
-			if (path.isFile()) {
-				String pathString = path.toString();
-				if(pathString.endsWith(".class")){
-					// get simple Name
-					String fileName = getSimpleFileName(pathString);
-					// strip fileextension
-					fileName = fileName.substring(0, fileName.lastIndexOf('.'));	
-					Class<?> cleanroomClass = null;
-					Class<?> studentClass = null;
-
-					try{
-						cleanroomClass = cleanroomLoader.loadClass(fileName);
-						studentClass = studentLoader.loadClass(fileName);
-
-					} catch (ClassNotFoundException cnfe) {
-						throw new Error("Error - class [" + cnfe.getMessage()+"] not found");
-					}
-					
-					compareClasses(cleanroomClass,studentClass);
-				}
+		for(String className : checkMap.keySet()){
+			Class<?> cleanroomClass = null;
+			Class<?> studentClass = null;
+			try{
+				cleanroomClass = cleanroomLoader.loadClass(className);	
+				studentClass = studentLoader.loadClass(className);
+			} catch (ClassNotFoundException cnfe) {	
+				throw new Error("Error - class [" + cnfe.getMessage()+"] not found");
 			}
+			compareClasses(cleanroomClass,studentClass);
 		}
+
+
 
 		// check if there were methods declared in @CompareInterface which could not be found
 		if(methodMap.size() != 0) {
-			System.err.println("The following methods declared in @CompareInterface could not be found in cleanroom:");
+			System.err.println("The following method(s) declared in @CompareInterface could not be found in cleanroom:");
 			for(String methodName : methodMap.keySet()){		
 				System.err.print(methodName+" ");
 			}
