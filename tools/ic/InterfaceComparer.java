2186ec1294fa815f7122fb0c1a3528efb457f0c3
Cuong Bui <cuong.bui@fau.de>
2015-05-12 10:46:34 +0200

    now able to check specified Fields and Methods or everything if nothing is specified, #12

--- tools/ic/InterfaceComparer.java
+++ tools/ic/InterfaceComparer.java
@@ -13,60 +13,134 @@ public class InterfaceComparer {
 	private static boolean error = false;
 	private static HashMap<String,HashMap<String,Boolean>> checkMap = null;
 
-	// check for specified cleanroom methods with student counterpart
-	private static void compareClasses(Class<?> cleanroomClass, Class<?> studentClass) {
-		HashMap<String,Boolean> methodMap = checkMap.get(cleanroomClass.getName());
-		// checkMethods
-		for(Method cleanroomMethod : cleanroomClass.getDeclaredMethods()){
-			// only compare if method was specified in @CompareInterface annotation
-			if(methodMap.get(cleanroomMethod.getName()) == null){
-				continue;
-			}
-
-			methodMap.remove(cleanroomMethod.getName());
-
-			Class<?>[] pTypes = cleanroomMethod.getParameterTypes();
-			Method studentMethod = null; 
-			
-			try{
-				studentMethod = studentClass.getMethod(cleanroomMethod.getName(),pTypes);
-			} catch (NoSuchMethodException nsme){
-				System.err.println("ERROR - Method " +cleanroomMethod + "["+cleanroomClass.getName()+"] does not exists in student code or does not match with student counterpart");
-				error = true;
-				continue;
-			}
-			
-			if(!cleanroomMethod.toGenericString().equals(studentMethod.toGenericString())) {
-				error = true;
-				System.err.println("ERROR - Method " +cleanroomMethod + "["+cleanroomClass.getName()+"] does not exists in student code or does not match with student counterpart");
-			}
-		}		
-		
-		// check if there were methods declared in @CompareInterface which could not be found
-		if(methodMap.size() != 0) {
-			System.err.println("Error - The following method(s) declared in @CompareInterface could not be found in cleanroom class [" + cleanroomClass.getName()+"]:");
-			for(String methodName : methodMap.keySet()){		
-				System.err.print(methodName+" ");
-			}
-			System.err.println("");
-			error = true;
+	// retrieves a Field from a Class
+	private static Field getField(Class<?> clazz, String name){
+		try{
+			Field field = clazz.getField(name);
+			return field;
+		} catch(NoSuchFieldException nsfe) {
+			// Do nothing
 		}
+	
+		return null;
 	}
 
+
+	// retrieves all Methods and put in a Map
+	private static HashMap<String,Method> getMethodMapForClass(Class<?> clazz){
+		HashMap<String,Method> methodMap = new HashMap<String,Method>();
+		for(Method method : clazz.getDeclaredMethods()){
+			methodMap.put(method.getName(),method);
+		}
+		return methodMap;
+	}
+
+	// checks two fields and print err msg. If error occurs false is returned
+	private static boolean checkField(Field cleanroomField, Field studentField, Class<?> cleanroomClass){
+		if(studentField == null){
+			System.err.println("ERROR - Field " +cleanroomField + "["+cleanroomClass.getName() +"]does not exists in student code");
+			return false;
+		}else{
+			if(!cleanroomField.toGenericString().equals(studentField.toGenericString())){
+				System.err.println("ERROR - Field " +cleanroomField + "["+cleanroomClass.getName() +"]does not match with student counterpart");
+			return false;
+			
+			}
+		}
+
+		return true;
+	}
+
+	private static boolean checkMethod(Method cleanroomMethod, Class<?> studentClass){
+		Method studentMethod = null;
+		try {	
+			studentMethod = studentClass.getMethod(cleanroomMethod.getName(),cleanroomMethod.getParameterTypes());
+			if(!cleanroomMethod.toGenericString().equals(studentMethod.toGenericString())){
+				System.err.println("ERROR - Method " +cleanroomMethod + "["+studentClass.getName() +"]does not match with student counterpart");
+				return false;
+
+			}
+		} catch (NoSuchMethodException nsme){
+			System.err.println("ERROR - Method " +cleanroomMethod + "["+studentClass.getName() +"]does not match or does not exists in student code");
+			return false;
+
+		}
+
+		return true;
+	}
+	
+	// checks everything
+	private static void checkAll(Class<?> cleanroomClass, Class<?> studentClass){
+		// check all Methods
+		HashMap<String,Method> studentMethodMap = getMethodMapForClass(studentClass);
+		for(Method cleanroomMethod : cleanroomClass.getDeclaredMethods()){
+			boolean success = checkMethod(cleanroomMethod,studentClass);
+			if(!success) {
+				error = true;
+			}
+
+		}
+
+
+		for(Field cleanroomField : cleanroomClass.getFields()){
+			boolean success = checkField(cleanroomField,getField(studentClass,cleanroomField.getName()),cleanroomClass);
+			if(!success) {
+				error = true;
+			}
+		}
+
+	}
+
+
+	// check for specified cleanroom methods with student counterpart
+	private static void compareClasses(Class<?> cleanroomClass, Class<?> studentClass) {
+		HashMap<String,Boolean> fieldMethodMap = checkMap.get(cleanroomClass.getName());
+		if(fieldMethodMap.size() == 0){
+			// only class was given
+			checkAll(cleanroomClass,studentClass);
+			return;
+		}
+		HashMap<String,Method> cleanroomMethodMap = getMethodMapForClass(cleanroomClass);
+
+		// Check all given values in @CompareInterface
+		for(String toCheck : fieldMethodMap.keySet()){
+			// check if its a field
+			Field cleanroomField = getField(cleanroomClass,toCheck);
+			if(cleanroomField != null){
+				// get student counterpart
+				Field studentField = getField(studentClass,toCheck);
+				boolean success = checkField(cleanroomField,studentField,cleanroomClass);
+				if(!success) {
+					error = true;
+				}
+				continue;
+			}
+
+			// check if its a method
+			Method cleanroomMethod = cleanroomMethodMap.get(toCheck);
+			boolean success = checkMethod(cleanroomMethod,studentClass);
+			if(!success) {
+				error = true;
+			}
+		}
+	}		
+		
 	// parses the cmd args and save it to HashMap
 	private static void valuesToMap(String[] annotationValue){
 		checkMap = new HashMap<String,HashMap<String,Boolean>>();
 		for(String arg : annotationValue){
 			if(arg.contains(".")){
 				String[] parts = arg.split("\\.");
-				HashMap<String,Boolean> methodMap = checkMap.get(parts[0]);
-				if(methodMap == null){
-					methodMap = new HashMap<String,Boolean>();
+				HashMap<String,Boolean> fieldMethodMap = checkMap.get(parts[0]);
+				if(fieldMethodMap == null){
+					fieldMethodMap = new HashMap<String,Boolean>();
 				}
-					methodMap.put(parts[1],true);
-					checkMap.put(parts[0],methodMap);
+					fieldMethodMap.put(parts[1],true);
+					checkMap.put(parts[0],fieldMethodMap);
 			} else {
-			
+				// Only Class is given
+				HashMap<String,Boolean> fieldMethodMap = new HashMap<String,Boolean>();
+				checkMap.put(arg,fieldMethodMap);
 			}
 
 		}
@@ -94,8 +168,6 @@ public class InterfaceComparer {
 			System.err.println("Usage: java tools.ic.InterfaceComparer JUnitTest");
 			System.exit(-1);
 		}
-
-
 
 		String cwd = System.getProperty("user.dir");
 		String pathToCleanroom = cwd + "/cleanroom/";
@@ -126,7 +198,6 @@ public class InterfaceComparer {
 			} catch (ClassNotFoundException cnfe) {	
 				throw new Error("Error - student class [" + cnfe.getMessage()+"] not found");
 			}
-	
 			
 			compareClasses(cleanroomClass,studentClass);
 		}
