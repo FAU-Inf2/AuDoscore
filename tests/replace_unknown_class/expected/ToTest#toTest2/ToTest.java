9d619fd9ffc884c9be215fa772ee6531fa078a84
Tobias Werth <werth@cs.fau.de>
2015-11-12 12:01:48 +0100

    Add testcase for #34.

---
+++ tests/replace_unknown_class/expected/ToTest#toTest2/ToTest.java
@@ -0,0 +1,15 @@
+
+public class ToTest {
+    
+    public ToTest() {
+        super();
+    }
+    
+    public static int toTest() {
+        return 23;
+    }
+    
+    public static int toTest2() {
+        return 23;
+    }
+}
---
+++ tests/replace_unknown_class/junit/SecretTest.java
@@ -0,0 +1,28 @@
+import org.junit.*;
+import tester.annotations.*;
+
+import static org.junit.Assert.*;
+
+@SecretClass
+public class SecretTest {
+	// instead of explicitly coding the following rules here,
+	// your test class can also just extend the class JUnitWithPoints
+	@Rule
+	public final PointsLogger pointsLogger = new PointsLogger();
+	@ClassRule
+	public final static PointsSummary pointsSummary = new PointsSummary();
+
+	@Test(timeout=100)
+	@Points(exID = "GA4.6a", bonus = 47.11)
+	@Replace({"ToTest.toTest"})
+	public void test() {
+		assertEquals("Should return 42", 42, ToTest.toTest());
+	}
+
+	@Test(timeout=100)
+	@Points(exID = "GA4.6a", bonus = 23.00)
+	@Replace({"UnkownClass.toTest2"})
+	public void test2() {
+		assertEquals("Should return 23", 23, ToTest.toTest2());
+	}
+}
