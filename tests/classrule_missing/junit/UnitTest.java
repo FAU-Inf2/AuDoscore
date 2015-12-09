497e0cbfeec46d076187677198d5c13a947d3b25
Tobias Werth <werth@cs.fau.de>
2015-12-09 14:22:10 +0100

    Add testcases for missing @Rule/@ClassRule.

---
+++ tests/classrule_missing/junit/UnitTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import tester.annotations.Ex;
+import tester.annotations.Exercises;
+import tester.annotations.Points;
+
+@Exercises({ @Ex(exID = "GA4.6a", points = 12.5)})
+public class UnitTest {
+//	// instead of explicitly coding the following rules here,
+//	// your test class can also just extend the class JUnitWithPoints
+	@Rule
+	public final PointsLogger pointsLogger = new PointsLogger();
+// XXX: @ClassRule intentionally to provoke error
+//	@ClassRule
+	public final static PointsSummary pointsSummary = new PointsSummary();
+
+	@Test(timeout=200)
+	@Points(exID = "GA4.6a", bonus = 47.11)
+	public void test() {
+		assertEquals("Should return 42", 42, ToTest.toTest());
+	}
+}
