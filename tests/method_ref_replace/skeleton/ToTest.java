69efd3363a175c1ddb40fdf16535443025a88485
Marius Kamp <marius.kamp@fau.de>
2015-12-10 12:48:32 +0100

    Add test case for method references

---
+++ tests/method_ref_replace/skeleton/ToTest.java
@@ -0,0 +1,16 @@
+import java.util.function.IntSupplier;
+
+public class ToTest {
+	public int foo() {
+		return 0;
+	}
+
+	public int bar(final IntSupplier sup) {
+		return 0;
+	}
+
+	public int baz() {
+		return 0;
+	}
+}
+
