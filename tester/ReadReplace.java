2ff347d5513d5d31faa656f52840a8c7b9d4c8ba
Marius Kamp <marius.kamp@fau.de>
2016-06-13 15:33:48 +0200

    Show internal error if secret test fails in <clinit>
    If an error occurred in the class initializer of the secret test, the
    error would not be reported until the Json parsing fails. This is fixed
    so that a "nice" internal error is shown. Also added a test case.

--- tester/ReadReplace.java
+++ tester/ReadReplace.java
@@ -109,7 +109,7 @@ public class ReadReplace {
 				}else{
 					System.out.println("echo \",\" 1>&2");
 				}
-				System.out.println("java -XX:+UseConcMarkSweepGC -Xmx1024m -cp lib/json-simple-1.1.1.jar:lib/junit.jar:lib/junitpoints.jar:" + classpath + ":. -Dpub=" +pub+" -Djson=yes tools.SingleMethodRunner " + tcln + " "  + method + " || echo");
+				System.out.println("java -XX:+UseConcMarkSweepGC -Xmx1024m -cp lib/json-simple-1.1.1.jar:lib/junit.jar:lib/junitpoints.jar:" + classpath + ":. -Dpub=" +pub+" -Djson=yes tools.SingleMethodRunner " + tcln + " "  + method);
 			}
 
 		}
