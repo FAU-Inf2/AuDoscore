fe759d2812db1c11f276473a707ea474b870230d
Marius Kamp <marius.kamp@fau.de>
2016-07-07 12:13:20 +0200

    Fix another bug in ReadForbidden

--- ReadForbidden.java
+++ ReadForbidden.java
@@ -29,7 +29,7 @@ public class ReadForbidden {
 			if (notforbidden == null) continue;
 			for (String s : notforbidden.value()) {
 				hasNotForbidden = true;
-				grep2.append(sep).append(getRegex(s, notforbidden.type()));
+				grep2.append(sep).append("\\W").append(getRegex(s, notforbidden.type()));
 				sep = "|";
 			}
 		}
@@ -53,7 +53,7 @@ public class ReadForbidden {
 				return classSpec.replaceAll("\\.", "(\\\\.|/)") + "(\\W|$)";
 
 			case WILDCARD:
-				return classSpec.replaceAll("\\.", "(\\\\.|/)").replaceAll("\\*", "\\S*");
+				return classSpec.replaceAll("\\.", "(\\\\.|/)").replaceAll("\\*", "[^\\\\./\\\\s]*");
 
 			default:
 				System.err.println("unsupported type for @Forbidden");
