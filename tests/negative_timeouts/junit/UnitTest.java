a1fc5bfbee1a7ba2e84eb4fc4df8b99cb81c4696
Marius Kamp <marius.kamp@fau.de>
2016-12-02 07:24:47 +0100

    Do not allow negative timeouts in tests

---
+++ tests/negative_timeouts/junit/UnitTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import tester.annotations.Ex;
+import tester.annotations.Exercises;
+import tester.annotations.Points;
+
+@Exercises({ @Ex(exID = "NegativeTimeouts", points = 2.0) })
+public class UnitTest {
+	@Rule
+	public final PointsLogger pointsLogger = new PointsLogger();
+	@ClassRule
+	public static final PointsSummary pointsSummary = new PointsSummary();
+
+	@Test(timeout=-5)
+	@Points(exID = "NegativeTimeouts", bonus = 0.00001)
+	public void test() {
+		assertEquals(42, ToTest.test());
+	}
+}
+
