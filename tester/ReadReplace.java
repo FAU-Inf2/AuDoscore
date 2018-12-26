3d50fe9298ef345f1e0dce743da39b5348fb1970
Marius Kamp <msk@posteo.org>
2018-12-26 17:51:40 +0100

    Add onlyIf element to Replace annotations
    The onlyIf element can be used to specify when to perform a replacement.
    In this way, situations can be avoided in which a student's submission
    with replaced parts would not compile.

--- tester/ReadReplace.java
+++ tester/ReadReplace.java
@@ -19,6 +19,8 @@ public class ReadReplace {
 		return methods;
 	}
 
+
+
 	public static String getCanonicalReplacement(Replace r) {
 		Map<String, SortedSet<String>> mMethsMap = getMap(r);
 		final StringBuilder ncln = new StringBuilder();
@@ -31,6 +33,8 @@ public class ReadReplace {
 		return ncln.toString();
 	}
 
+
+
 	public static String getCanonicalReplacement(Description description) {
 		if (description.getAnnotation(Replace.class) != null) {
 			Replace r = description.getAnnotation(Replace.class);
@@ -38,6 +42,8 @@ public class ReadReplace {
 		}
 		return "";
 	}
+
+
 
 	public static Map<String, SortedSet<String>> getMap(Replace r) {
 		Map<String, SortedSet<String>> mMethsMap = new TreeMap<>();
@@ -80,6 +86,124 @@ public class ReadReplace {
 		return mMethsMap;
 	}
 
+
+
+	private static Class<?> getClassFromDescriptor(final String[] descParts) {
+		final String[] classParts = new String[descParts.length - 1];
+		System.arraycopy(descParts, 0, classParts, 0, classParts.length);
+
+		try {
+			return Class.forName(String.join(".", classParts));
+		} catch (final ClassNotFoundException e) {
+			throw new AnnotationFormatError("ERROR - Class '"
+					+ String.join(".", classParts)
+					+ "' does not exist");
+		}
+	}
+
+
+
+	private static Field getFieldFromDescriptor(final String[] descParts) {
+		final Class<?> cls = getClassFromDescriptor(descParts);
+		try {
+			return cls.getDeclaredField(descParts[descParts.length - 1]);
+		} catch (final NoSuchFieldException e) {
+			return null;
+		}
+	}
+
+
+
+	private static List<Method> getMethodsFromDescriptor(final String[] descParts) {
+		final Class<?> cls = getClassFromDescriptor(descParts);
+		final List<Method> result = new ArrayList<>();
+		for (final Method method : cls.getDeclaredMethods()) {
+			if (method.getName().equals(descParts[descParts.length - 1])) {
+				result.add(method);
+			}
+		}
+		return result;
+	}
+
+
+
+	private static Class<?> getTypeFromDescriptor(final String desc) {
+		if (desc.endsWith("[]")) {
+			final Class<?> componentType = getTypeFromDescriptor(desc.substring(0, desc.length() - 2));
+			return Array.newInstance(componentType, 0).getClass();
+		}
+
+		switch (desc) {
+			case "byte":    return byte.class;
+			case "boolean": return boolean.class;
+			case "char":    return char.class;
+			case "double":  return double.class;
+			case "float":   return float.class;
+			case "int":     return int.class;
+			case "long":    return long.class;
+			case "short":   return short.class;
+			default: {
+				try {
+					return Class.forName(desc);
+				} catch (final ClassNotFoundException e) {
+					throw new AnnotationFormatError("ERROR - Invalid type descriptor: " + desc);
+				}
+			}
+		}
+	}
+
+
+
+	private static boolean canProcess(final Replace replace) {
+		if (replace.onlyIf() == null || replace.onlyIf().isEmpty()) {
+			return true;
+		}
+
+		final String[] parts = replace.onlyIf().split(";");
+		if (parts.length < 3) {
+			throw new AnnotationFormatError("ERROR - Invalid 'onlyIf' value: " + replace.onlyIf());
+		}
+
+		final String[] descParts = parts[1].split("\\.");
+		if (descParts.length < 2) {
+			throw new AnnotationFormatError("ERROR - Invalid 'onlyIf' descriptor: " + parts[1]);
+		}
+
+		switch (parts[0]) {
+			case "field": {
+				final Field field = getFieldFromDescriptor(descParts);
+				if (field == null) {
+					return false;
+				}
+				return field.getType().equals(getTypeFromDescriptor(parts[2]));
+			}
+
+			case "method": {
+				final List<Method> methods = getMethodsFromDescriptor(descParts);
+				outer:
+				for (final Method method : methods) {
+					if (parts.length - 3 == method.getParameterCount()
+							&& method.getReturnType().equals(getTypeFromDescriptor(parts[2]))) {
+
+						int i = 0;
+						for (final Class<?> paramType : method.getParameterTypes()) {
+							if (!paramType.equals(getTypeFromDescriptor(parts[3 + i++]))) {
+								continue outer;
+							}
+						}
+						return true;
+					}
+				}
+				return false;
+			}
+
+			default:
+				throw new AnnotationFormatError("ERROR - Invalid 'onlyIf' type: " + parts[0]);
+		}
+	}
+
+
+
 	public static void loopSecret(String tcln, String pub) throws Exception {
 		LinkedHashMap<String,List<String>> rmap = new LinkedHashMap<String,List<String>>();
 		ClassLoader cl = ClassLoader.getSystemClassLoader();
@@ -87,13 +211,15 @@ public class ReadReplace {
 		for (Method meth : getMethodsSorted(c)) {
 			if (meth.isAnnotationPresent(Replace.class)) {
 				Replace r = meth.getAnnotation(Replace.class);
-				String cr = getCanonicalReplacement(r);
-				List<String> methods = rmap.get(cr);
-				if (methods == null) {
-					methods = new ArrayList<String>();
+				if (canProcess(r)) {
+					String cr = getCanonicalReplacement(r);
+					List<String> methods = rmap.get(cr);
+					if (methods == null) {
+						methods = new ArrayList<String>();
+					}
+					methods.add(meth.getName());
+					rmap.put(cr, methods);
 				}
-				methods.add(meth.getName());
-				rmap.put(cr, methods);
 			}
 		}
 
@@ -141,33 +267,35 @@ public class ReadReplace {
 		for (Method meth : getMethodsSorted(c)) {
 			if (meth.isAnnotationPresent(Replace.class)) {
 				Replace r = meth.getAnnotation(Replace.class);
-				Map<String, SortedSet<String>> methsMap = getMap(r);
-				for (Map.Entry<String, SortedSet<String>> e : methsMap.entrySet()) {
-					final StringBuilder ncln = new StringBuilder(e.getKey());
-					if(e.getValue().size() == 0) {
-						continue;
+				if (canProcess(r)) {
+					Map<String, SortedSet<String>> methsMap = getMap(r);
+					for (Map.Entry<String, SortedSet<String>> e : methsMap.entrySet()) {
+						final StringBuilder ncln = new StringBuilder(e.getKey());
+						if(e.getValue().size() == 0) {
+							continue;
+						}
+						for(String me : e.getValue()) {
+							ncln.append('#').append(me.replaceAll("<", "\\\\<").replaceAll(">", "\\\\>"));
+						}
+						final String nclns = ncln.toString();
+						mids.add("mkdir -p " + nclns + "; "
+								+ "javac -Xprefer:source -cp .:lib/junit.jar:lib/junitpoints.jar -Areplaces="
+								+ nclns
+								+ " -proc:only -processor ReplaceMixer cleanroom/"
+								+ e.getKey() + ".java "
+								+ e.getKey() + ".java > "
+								+ nclns
+								+ "/"
+								+ e.getKey() + ".java; "
+								+ "javac -cp . -d "
+								+ nclns
+								+ " -sourcepath "
+								+ nclns
+								+ " "
+								+ nclns
+								+ "/"
+								+ e.getKey() + ".java;");
 					}
-					for(String me : e.getValue()) {
-						ncln.append('#').append(me.replaceAll("<", "\\\\<").replaceAll(">", "\\\\>"));
-					}
-					final String nclns = ncln.toString();
-					mids.add("mkdir -p " + nclns + "; "
-							+ "javac -Xprefer:source -cp .:lib/junit.jar:lib/junitpoints.jar -Areplaces="
-							+ nclns
-							+ " -proc:only -processor ReplaceMixer cleanroom/"
-							+ e.getKey() + ".java "
-							+ e.getKey() + ".java > "
-							+ nclns
-							+ "/"
-							+ e.getKey() + ".java; "
-							+ "javac -cp . -d "
-							+ nclns
-							+ " -sourcepath "
-							+ nclns
-							+ " "
-							+ nclns
-							+ "/"
-							+ e.getKey() + ".java;");
 				}
 			}
 		}
--- tester/annotations/Replace.java
+++ tester/annotations/Replace.java
@@ -9,4 +9,5 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Replace{
 	String[] value();
+	String onlyIf() default "";
 }
